c1d85c74767295548516b48789c358f4befeee69
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-03 10:52:30 +0330

    update blog home and single: + remove parts that I don't need + fix linking from home to single + show my data on single page

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', index_view, name = 'index'),
-    path('/single/', single_view, name='single'),
-    path('/post-<int:pid>', test, name='test')
+    path('/<int:pid>', single_view, name='single'),
+    #path('/post-<int:pid>', test, name='test')
 ]
