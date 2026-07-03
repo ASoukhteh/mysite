812982240bd75223612de8ef26b5ab8cd474d411
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-03 09:20:50 +0330

    show my posts in blog page

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from blog.models import Post
 
 def index_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 def single_view(request):
     return render(request, 'blog/blog-single.html')
