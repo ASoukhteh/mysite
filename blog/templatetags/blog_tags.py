bdcdd800c82dc0e10293e120aea93746564779c3
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-05 16:21:53 +0330

    create blog tag for latest post use inclusion_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,13 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+@register.simple_tag
+def function():
+    return "hello"
+
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts': posts}
