4f6fb0b1a1413f63856a2f52870c30d9e0bfa89f
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-08 11:48:14 +0330

    sitemaps for static views

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,9 +41,13 @@ INSTALLED_APPS = [
 
     'django.contrib.humanize',
     'django_extensions',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 
     'blog'
 ]
+
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
