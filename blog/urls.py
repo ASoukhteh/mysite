284399fb745316f877c1fb01eb67c17a86ca815e
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-08 16:03:10 +0330

    taggit module

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:pid>/', single_view, name='single'),
     path('category/<str:cat_name>/', index_view, name='category'),
     path('author/<str:author_username>/', index_view, name='author'),
+    path('tag/<str:tag_name>/', index_view, name='tag'),
     path('search/', blog_search, name='search'),
     path('test', test, name='test')
 ]
