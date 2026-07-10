fd7664a402a590fe9e4d7979528fa5dc2f63da97
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-10 12:59:40 +0330

    fix login and logout to work properly

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', views.login_view, name='login'), 
-    # path('logout', views.login_view, name='logout'), 
-    path('signup', views.login_view, name='signup'), 
+    path('login/', views.login_view, name='login'), 
+    path('logout/', views.logout_view, name='logout'), 
+    path('signup/', views.login_view, name='signup'), 
 ]
