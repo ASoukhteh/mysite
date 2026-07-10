05ce3ce2ca799f047f3e89e80b309ec0b5eb15fc
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-10 16:59:54 +0330

    fix sign up

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login'), 
     path('logout/', views.logout_view, name='logout'), 
-    path('signup/', views.login_view, name='signup'), 
+    path('signup/', views.signup_view, name='signup'), 
 ]
